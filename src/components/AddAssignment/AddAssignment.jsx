25d778dc1d7266c593c48e021a0bcb5ee5c02355
recipra <jmo0425@outlook.com>
2022-07-26 13:49:00 -0400

    set default value to due date input and adjust handleChange function

--- src/components/AddAssignment/AddAssignment.jsx
+++ src/components/AddAssignment/AddAssignment.jsx
@@ -2,27 +2,33 @@ import { useState } from 'react'
 import styles from './AddAssignment.module.css'
 
 const AddAssignment = () => {
-  const [formData, setFormData] = useState()
+  const [formData, setFormData] = useState({
+    name: '',
+    dueDate: '',
+    estTime: 0
+  })
+
+  const currentDate = new Date()
+  currentDate.setDate(currentDate.getDate() + 1)
+  const date = currentDate.toISOString().substring(0, 10)
 
   const handleChange = (event) => {
-    setFormData([...formData, event.target.value])
+    setFormData({...formData, [event.target.name]: event.target.value})
   }
 
   const handleSubmit = async (event) => {
     event.preventDefault()
   }
 
-  console.log(formData)
-
   return (
     <div className={styles.assignmentForm}>
       <form autoComplete='off' onSubmit={handleSubmit}>
         <p>Assignment Name:</p>
-        <input type="text" placeholder='Enter the assignment name' onChange={handleChange}/>
+        <input type="text" placeholder='Ex. Arrays Lab' name='name' onChange={handleChange}/>
         <p>Due Date:</p>
-        <input type="text" placeholder='Enter the due date' onChange={handleChange}/>
-        <p>Est. Time:</p>
-        <input type="number" placeholder='Enter the time it will take to complete' onChange={handleChange}/><br />
+        <input type="date" placeholder='Enter the due date' name='dueDate' defaultValue={date} onChange={handleChange}/>
+        <p>Est. Time (hours):</p>
+        <input type="number" placeholder='Ex. 2' name='estTime' onChange={handleChange}/><br />
         <button type='submit'>Add Assignment</button>
       </form>
     </div>
